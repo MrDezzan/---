2e789ae7d410208b07c39d1b2797b42174352658
MrDezzan <160172433+MrDezzan@users.noreply.github.com>
2024-04-17 19:43:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
-import time
+import discord
 import random
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "ИМБА": "Что-то слишком крутое"
-            }
-while True:
-    time.sleep(0.2)
-    print("Выберите опцию:")
-    time.sleep(0.2)
-    print("1 - Добавить слово")
-    time.sleep(0.2)
-    print("2 - Просмотреть слова")
-    time.sleep(0.2)
-    print("3 - Рандомное слово")
-    time.sleep(0.2)
-    print("4 - Удалить")
-    n = int(input())
-    if n == 1:
-        key=input("Введите слово: ")
-        value=input("Введите объяснение: ")
-        meme_dict[key] = value
-    if n == 2:
-        print(meme_dict)
-    if n == 3:
-        randkey = random.choice(meme_dict.keys())
-        randval = meme_dict[randkey]
-        print(randkey, " - ", randval)
-    if n == 4:
-        print("Введите название ключа: ")
-        ke = input()
-        del meme_dict[ke]
+from discord.ext import commands
+import re
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We havssse logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def python(ctx):
+    await ctx.send('Я тест-бот для курса Python Pro')
+@bot.command()
+async def bc(ctx, channel: discord.TextChannel, *, message):
+    await channel.send(f'@everyone {message}')
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    for i in range(times):
+        await ctx.send(content)
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+bot.run("")
